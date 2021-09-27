afdb73e0cdd865f39a9aa30e35346145b6143a3c
user <email>
2021-09-27 20:31:33 +0300

    Add ContactRepository

--- src/main/java/app/repository/ContactRepository.java
+++ src/main/java/app/repository/ContactRepository.java
@@ -1,2 +1,54 @@
-package app.repository;public class ContactRepository {
+package app.repository;
+
+import app.entity.Contact;
+import app.holder.SessionFactoryHolder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class ContactRepository
+{
+    private final SessionFactory sessionFactory;
+    public ContactRepository()
+    {
+        this.sessionFactory=SessionFactoryHolder.getSessionFactory();
+    }
+    public List<Contact> list()
+    {
+        Session session=sessionFactory.openSession();
+        List<Contact> list=session.createQuery("from Contact").list();
+        session.close();
+        return list;
+    }
+    public Contact save(Contact contact)
+    {
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(contact);
+        session.getTransaction().commit();
+        return contact;
+    }
+    public Contact update(Contact contact)
+    {
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(contact);
+        session.getTransaction().commit();
+        return contact;
+    }
+    public void delete(Contact contact)
+    {
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(contact);
+        session.getTransaction().commit();
+    }
+    public Contact getById(long id)
+    {
+        Session session=sessionFactory.openSession();
+        Contact contact=session.get(Contact.class,id);
+        session.close();
+        return contact;
+    }
 }
