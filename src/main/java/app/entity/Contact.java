362b9be0fe79be7598a351c8e285b65e227eda44
user <email>
2021-09-27 19:24:23 +0300

    Add dependencies

---
+++ src/main/java/app/entity/Contact.java
@@ -0,0 +1,39 @@
+package app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name,phone;
+    public long getId()
+    {
+        return id;
+    }
+    public void setId(long id)
+    {
+        this.id=id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public String getPhone()
+    {
+        return phone;
+    }
+    public void setPhone(String phone)
+    {
+        this.phone=phone;
+    }
+}
